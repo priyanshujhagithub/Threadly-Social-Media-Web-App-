3dc75c51f337d041a08ed74e2a1a25db9af6f83d
priyanshujhagithub <129042608+priyanshujhagithub@users.noreply.github.com.>
2025-04-14 10:32:35 +0530

    Root Route for Testing Added

--- server/index.js
+++ server/index.js
@@ -32,6 +32,11 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
+/* ROOT ROUTE FOR TESTING */
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
+});
+
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
