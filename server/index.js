0bd28e5e4df53d5659834158fcc29844f124c660
priyanshujhagithub <129042608+priyanshujhagithub@users.noreply.github.com.>
2025-05-06 12:30:49 +0530

    minor changes-5

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin: 'https://threadly-social-media-frontend.up.railway.app',
+  origin: 'https://bubbly-endurance-production.up.railway.app',
   credentials: true,
 }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
